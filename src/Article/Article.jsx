7d19c1be0a7ae0756398ced71a6bafcb9f402c5b
Tom Bryson <tom@Toms-Mac-mini-2.lan>
2023-12-12 10:30:07 +0000

    add individual article view (and Home button)

--- src/Article/Article.jsx
+++ src/Article/Article.jsx
@@ -1,6 +1,7 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { getArticleById } from "../api";
+import "./article.css"
 
 
 export default function Article({article, setArticle}) {
@@ -18,14 +19,19 @@ useEffect(() => {
 if (loading){
     return <h1>Loading...</h1>
 } else return (
-    <>
-      <h1>{article.title}</h1>
+  <div id="article-container">
+    <div id="article-header">
+      <div id="article-topic-head">{article.topic} </div> <div id="article-date">{article.created_at.slice(0, -14)}</div>
+    </div>
+    <h1>{article.title}</h1>
+    <div id="author-container">
       <p>{article.author}</p>
-      <img src={article.article_img_url}/>
-      <p>{article.body}</p>
-      {console.log(article)}
-      <h2>Comments ({article.comment_count})</h2>
-      <div>Comments component will go here</div>
-    </>
-  );
+    </div>
+    <img id="article-image"src={article.article_img_url} />
+    <p>{article.body}</p>
+    {console.log(article)}
+    <h2>Comments ({article.comment_count})</h2>
+    <div>Comments component will go here</div>
+  </div>
+);
 }
